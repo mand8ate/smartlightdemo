e849b581f7f77ba11ee63653a8dfa83f5f5eb639
mand8ate <mand8ate@gmail.com>
2024-05-16 19:36:58 +0900

    change to useEffectin OnOffSwitcher

--- src/components/OnOffSwitcher.tsx
+++ src/components/OnOffSwitcher.tsx
@@ -36,7 +36,7 @@ export default function OnOffSwitch({ deviceId }: OnOffSwitchProps) {
     };
 
     fetchData();
-  }, []);
+  });
 
   const handleToggle = async () => {
     const newChecked = !checked;
